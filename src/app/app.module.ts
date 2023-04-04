04625943fd160e201b02a4b1aa922a9832f9cc7e
PEREYRA ESPECHE, Fabricio Daniel <fabriciopereyra@gmail.com>
2023-04-04 19:56:15 -0300

    Creados los componentes principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { SocialComponent } from './components/main/social/social.component';
+import { HeroComponent } from './components/main/hero/hero.component';
+import { AboutmeComponent } from './components/main/aboutme/aboutme.component';
+import { ExperienceComponent } from './components/main/experience/experience.component';
+import { EducationComponent } from './components/main/education/education.component';
+import { SkillComponent } from './components/main/skill/skill.component';
+import { ProjectComponent } from './components/main/project/project.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    ModalComponent,
+    SocialComponent,
+    HeroComponent,
+    AboutmeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillComponent,
+    ProjectComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
